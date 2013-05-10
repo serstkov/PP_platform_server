5264310a282ec8c9d2acf74006d336de69546555
Justas Serstkovas <serstkov@me.com>
2013-05-10 19:19:05 +0100

    minor test data changes

--- JSON_Feed_v.1.php
+++ JSON_Feed_v.1.php
@@ -7,7 +7,7 @@
  class Module  {
  	 public $id = "";
 	 public $title = "";
-     public $youtube_video_url = "";
+     public $youtube_video_id = "";
 	 public $audio_url = "";
 	 public $text = "";
 	 
@@ -17,6 +17,9 @@ $module = new Module();
 
 $module->id = 1;
 $module->title = "test title";
+$module->youtube_video_id = "5XMVLbUvr4w";
+$module->audio_url = "/audio/5XMVLbUvr4w.mp3";
+$module->text = "our test text here";
 
  // //$data = array(4 => "four", 8 => "eight");
 //  
